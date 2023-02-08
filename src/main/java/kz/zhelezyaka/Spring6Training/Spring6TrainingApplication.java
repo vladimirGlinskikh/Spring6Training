956bfdc88d70547061e1c72854fd8703912c32b3
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-02-08 21:47:27 +0300

    Start project

---
+++ src/main/java/kz/zhelezyaka/Spring6Training/Spring6TrainingApplication.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.Spring6Training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6TrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6TrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/zhelezyaka/Spring6Training/Spring6TrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.Spring6Training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6TrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
