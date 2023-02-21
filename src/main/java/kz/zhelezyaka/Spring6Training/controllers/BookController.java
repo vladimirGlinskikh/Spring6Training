87f1388b51e3602efc8e9bc00fe516e89e698f0e
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-02-21 19:41:54 +0300

    Configuring Spring Controllers

---
+++ src/main/java/kz/zhelezyaka/Spring6Training/controllers/BookController.java
@@ -0,0 +1,21 @@
+package kz.zhelezyaka.Spring6Training.controllers;
+
+import kz.zhelezyaka.Spring6Training.services.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookService.findAll());
+        return "books";
+    }
+}
