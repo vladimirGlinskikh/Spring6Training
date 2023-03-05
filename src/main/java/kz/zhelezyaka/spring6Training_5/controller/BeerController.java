a79ab9a2417dd77f5f100ae7e8562217f7d8bd07
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-03-05 18:19:35 +0300

    Set Header on HTTP Response

--- src/main/java/kz/zhelezyaka/spring6Training_5/controller/BeerController.java
+++ src/main/java/kz/zhelezyaka/spring6Training_5/controller/BeerController.java
@@ -4,6 +4,7 @@ import kz.zhelezyaka.spring6Training_5.model.Beer;
 import kz.zhelezyaka.spring6Training_5.services.BeerServices;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,8 +22,12 @@ public class BeerController {
     @PostMapping
 //    @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity handlePost(@RequestBody Beer beer) {
-        Beer saveBeer = beerServices.saveNewBeer(beer);
-        return new ResponseEntity(HttpStatus.CREATED);
+        Beer savedBeer = beerServices.saveNewBeer(beer);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location",
+                "/api/v1/beer/" +
+                        savedBeer.getId().toString());
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
     @RequestMapping(method = RequestMethod.GET)
