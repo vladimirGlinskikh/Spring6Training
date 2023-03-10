08f06cb1d02095a07072b97d720a78390c313e94
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-03-10 14:15:01 +0300

    Using JSON Matchers

--- src/test/java/kz/zhelezyaka/spring6Training_5/controller/BeerControllerTest.java
+++ src/test/java/kz/zhelezyaka/spring6Training_5/controller/BeerControllerTest.java
@@ -1,6 +1,8 @@
 package kz.zhelezyaka.spring6Training_5.controller;
 
+import kz.zhelezyaka.spring6Training_5.model.Beer;
 import kz.zhelezyaka.spring6Training_5.services.BeerServices;
+import kz.zhelezyaka.spring6Training_5.services.BeerServicesImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,7 +12,10 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BeerController.class)
@@ -21,10 +26,18 @@ class BeerControllerTest {
     @MockBean
     BeerServices beerServices;
 
+    BeerServicesImpl beerServicesImpl = new BeerServicesImpl();
+
     @Test
     void getBeerById() throws Exception {
+        Beer testBeer = beerServicesImpl.listBeers().get(0);
+
+        given(beerServices.getBeerById(any(UUID.class)))
+                .willReturn(testBeer);
+
         mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID())
                         .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 }
