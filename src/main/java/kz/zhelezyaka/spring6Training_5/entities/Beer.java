c80cb32e4ca1a0763f588974a174f16528cdb0b0
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-03-17 18:16:11 +0300

    Hibernate UUID Id Generation

--- src/main/java/kz/zhelezyaka/spring6Training_5/entities/Beer.java
+++ src/main/java/kz/zhelezyaka/spring6Training_5/entities/Beer.java
@@ -1,9 +1,12 @@
 package kz.zhelezyaka.spring6Training_5.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import kz.zhelezyaka.spring6Training_5.model.BeerStyle;
 import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -17,6 +20,9 @@ import java.util.UUID;
 @NoArgsConstructor
 public class Beer {
     @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
     private UUID id;
     private Integer version;
     private String beerName;
--- src/main/java/kz/zhelezyaka/spring6Training_5/entities/Customer.java
+++ src/main/java/kz/zhelezyaka/spring6Training_5/entities/Customer.java
@@ -1,8 +1,11 @@
 package kz.zhelezyaka.spring6Training_5.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -15,6 +18,9 @@ import java.util.UUID;
 @NoArgsConstructor
 public class Customer {
     @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
     private UUID id;
     private String customerName;
     private Integer version;
