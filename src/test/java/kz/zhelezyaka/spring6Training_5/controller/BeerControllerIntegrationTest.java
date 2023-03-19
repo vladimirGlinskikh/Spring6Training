1209fa231b333c9f5f0e654082b7278514531e70
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-03-19 16:11:40 +0300

    Testing for expected Exceptions

--- src/test/java/kz/zhelezyaka/spring6Training_5/controller/BeerControllerIntegrationTest.java
+++ src/test/java/kz/zhelezyaka/spring6Training_5/controller/BeerControllerIntegrationTest.java
@@ -1,6 +1,8 @@
 package kz.zhelezyaka.spring6Training_5.controller;
 
 import jakarta.transaction.Transactional;
+import kz.zhelezyaka.spring6Training_5.entities.Beer;
+import kz.zhelezyaka.spring6Training_5.exceptions.NotFoundException;
 import kz.zhelezyaka.spring6Training_5.model.BeerDTO;
 import kz.zhelezyaka.spring6Training_5.repositories.BeerRepository;
 import org.junit.jupiter.api.Test;
@@ -9,8 +11,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class BeerControllerIntegrationTest {
@@ -18,6 +22,20 @@ class BeerControllerIntegrationTest {
     BeerController beerController;
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testBeerIdNotFound() {
+        assertThrows(NotFoundException.class, () -> {
+            beerController.getBeerById(UUID.randomUUID());
+        });
+    }
+
+    @Test
+    void testGetById() {
+        Beer beer = beerRepository.findAll().get(0);
+        BeerDTO dto = beerController.getBeerById(beer.getId());
+        assertThat(dto).isNotNull();
+    }
 
     @Test
     void testListBeers() {
