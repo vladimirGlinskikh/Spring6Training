264186e07e7923a5ec9bb708b0ddc97785004371
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-03-23 17:52:13 +0300

    Custom Validation Handler

---
+++ src/main/java/kz/zhelezyaka/spring6Training_5/controller/CustomErrorController.java
@@ -0,0 +1,15 @@
+package kz.zhelezyaka.spring6Training_5.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomErrorController {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity handleBindErrors(MethodArgumentNotValidException exception) {
+        return ResponseEntity.badRequest().body(exception.getBindingResult().getFieldError());
+    }
+}
--- src/test/java/kz/zhelezyaka/spring6Training_5/controller/BeerControllerTest.java
+++ src/test/java/kz/zhelezyaka/spring6Training_5/controller/BeerControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -59,11 +60,12 @@ class BeerControllerTest {
         given(beerService.saveNewBeer(any(BeerDTO.class)))
                 .willReturn(beerServicesImpl.listBeers().get(1));
 
-        mockMvc.perform(post(BeerController.BEER_PATH)
+        MvcResult mvcResult = mockMvc.perform(post(BeerController.BEER_PATH)
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(beerDTO)))
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isBadRequest()).andReturn();
+        System.out.println(mvcResult.getResponse().getContentAsString());
     }
 
     @Test
