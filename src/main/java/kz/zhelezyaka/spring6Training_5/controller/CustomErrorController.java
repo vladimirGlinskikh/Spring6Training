ba9dfd60db3f2bb80bd89b811fb8e0e6918be8bd
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-03-23 18:14:35 +0300

    Custom Error Body

--- src/main/java/kz/zhelezyaka/spring6Training_5/controller/CustomErrorController.java
+++ src/main/java/kz/zhelezyaka/spring6Training_5/controller/CustomErrorController.java
@@ -5,11 +5,23 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @ControllerAdvice
 public class CustomErrorController {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     ResponseEntity handleBindErrors(MethodArgumentNotValidException exception) {
-        return ResponseEntity.badRequest().body(exception.getBindingResult().getFieldError());
+        List<Map<String, String>> errorList = exception.getFieldErrors().stream()
+                .map(fieldError -> {
+                    Map<String, String> errorMap = new HashMap<>();
+                    errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+                    return errorMap;
+                }).collect(Collectors.toList());
+
+        return ResponseEntity.badRequest().body(errorList);
     }
 }
