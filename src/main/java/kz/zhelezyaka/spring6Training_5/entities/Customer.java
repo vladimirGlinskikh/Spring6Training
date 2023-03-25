8b5d8c05c56433386c5d88c52eb151e3c1f5b370
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-03-25 17:46:25 +0300

    JPA Updates for MySQL

--- src/main/java/kz/zhelezyaka/spring6Training_5/entities/Customer.java
+++ src/main/java/kz/zhelezyaka/spring6Training_5/entities/Customer.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -20,7 +22,8 @@ public class Customer {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
     private String customerName;
     private Integer version;
