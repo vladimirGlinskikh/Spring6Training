a1053dc2a03255628c534d4f5db09233962d7121
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-03-27 17:11:17 +0300

    Fix Integration Tests

--- src/test/java/kz/zhelezyaka/spring6Training_5/bootstrap/BootstrapDataTest.java
+++ src/test/java/kz/zhelezyaka/spring6Training_5/bootstrap/BootstrapDataTest.java
@@ -3,14 +3,17 @@ package kz.zhelezyaka.spring6Training_5.bootstrap;
 import kz.zhelezyaka.spring6Training_5.repositories.BeerRepository;
 import kz.zhelezyaka.spring6Training_5.repositories.CustomerRepository;
 import kz.zhelezyaka.spring6Training_5.services.BeerCsvService;
+import kz.zhelezyaka.spring6Training_5.services.BeerCsvServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@Import(BeerCsvServiceImpl.class)
 class BootstrapDataTest {
 
     @Autowired
@@ -31,7 +34,7 @@ class BootstrapDataTest {
     @Test
     void testRun() throws Exception {
         bootstrapData.run(null);
-        assertThat(beerRepository.count()).isEqualTo(3);
+        assertThat(beerRepository.count()).isEqualTo(2413);
         assertThat(customerRepository.count()).isEqualTo(3);
     }
 }
--- src/test/java/kz/zhelezyaka/spring6Training_5/controller/BeerControllerIntegrationTest.java
+++ src/test/java/kz/zhelezyaka/spring6Training_5/controller/BeerControllerIntegrationTest.java
@@ -153,7 +153,7 @@ class BeerControllerIntegrationTest {
     @Test
     void testListBeers() {
         List<BeerDTO> dtos = beerController.listBeers();
-        assertThat(dtos.size()).isEqualTo(3);
+        assertThat(dtos.size()).isEqualTo(2413);
     }
 
     @Rollback
