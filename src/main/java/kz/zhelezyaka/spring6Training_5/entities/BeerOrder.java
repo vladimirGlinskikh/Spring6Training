3b6f8e0ec1d583c106a60fff5612bb37312ed24f
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-04-03 16:09:50 +0300

    One to One Bi-Directional

--- src/main/java/kz/zhelezyaka/spring6Training_5/entities/BeerOrder.java
+++ src/main/java/kz/zhelezyaka/spring6Training_5/entities/BeerOrder.java
@@ -21,7 +21,7 @@ public class BeerOrder {
 
     public BeerOrder(UUID id, Long version, Timestamp createdDate,
                      Timestamp lastModifiedDate, String customerRef,
-                     Customer customer, Set<BeerOrderLine> beerOrderLines) {
+                     Customer customer, Set<BeerOrderLine> beerOrderLines, BeerOrderShipment beerOrderShipment) {
         this.id = id;
         this.version = version;
         this.createdDate = createdDate;
@@ -29,6 +29,7 @@ public class BeerOrder {
         this.customerRef = customerRef;
         this.setCustomer(customer);
         this.beerOrderLines = beerOrderLines;
+        this.beerOrderShipment = beerOrderShipment;
     }
 
     @Id
@@ -64,4 +65,7 @@ public class BeerOrder {
 
     @OneToMany(mappedBy = "beerOrder")
     private Set<BeerOrderLine> beerOrderLines;
+
+    @OneToOne
+    private BeerOrderShipment beerOrderShipment;
 }
