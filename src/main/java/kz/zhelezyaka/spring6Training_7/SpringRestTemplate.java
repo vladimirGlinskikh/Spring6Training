13499233216074fcba3b8d068aae548e8ee7c02e
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-04-06 20:58:24 +0300

    Create Rest Template project

---
+++ src/main/java/kz/zhelezyaka/spring6Training_7/SpringRestTemplate.java
@@ -0,0 +1,11 @@
+package kz.zhelezyaka.spring6Training_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestTemplate {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestTemplate.class, args);
+    }
+}
---
+++ src/main/java/kz/zhelezyaka/spring6Training_7/model/BeerStyle.java
@@ -0,0 +1,5 @@
+package kz.zhelezyaka.spring6Training_7.model;
+
+public enum BeerStyle {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
