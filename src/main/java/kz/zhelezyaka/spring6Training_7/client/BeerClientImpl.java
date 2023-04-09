1a3ebfa736b3fef8a266ee9275389182da7e9c58
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-04-09 21:39:51 +0300

    Get List as JSON String

--- src/main/java/kz/zhelezyaka/spring6Training_7/client/BeerClientImpl.java
+++ src/main/java/kz/zhelezyaka/spring6Training_7/client/BeerClientImpl.java
@@ -1,11 +1,25 @@
 package kz.zhelezyaka.spring6Training_7.client;
 
 import kz.zhelezyaka.spring6Training_7.model.BeerDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+@RequiredArgsConstructor
+@Service
 public class BeerClientImpl implements BeerClient {
+
+    private final RestTemplateBuilder restTemplateBuilder;
+
     @Override
     public Page<BeerDTO> listBeers() {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        ResponseEntity<String> stringResponse =
+                restTemplate.getForEntity("http://localhost:8080/api/v1/beer", String.class);
+        System.out.println(stringResponse.getBody());
         return null;
     }
 }
--- src/test/java/kz/zhelezyaka/spring6Training_7/client/BeerClientImplTest.java
+++ src/test/java/kz/zhelezyaka/spring6Training_7/client/BeerClientImplTest.java
@@ -1,12 +1,17 @@
 package kz.zhelezyaka.spring6Training_7.client;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class BeerClientImplTest {
+
+    @Autowired
+    BeerClientImpl beerClient;
 
     @Test
     void listBeers() {
+        beerClient.listBeers();
     }
 }
