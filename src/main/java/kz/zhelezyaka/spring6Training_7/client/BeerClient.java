a146663038785938fa443a73f2bbd8ee5807147c
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-04-15 18:23:16 +0300

    URL Parameters

--- src/main/java/kz/zhelezyaka/spring6Training_7/client/BeerClient.java
+++ src/main/java/kz/zhelezyaka/spring6Training_7/client/BeerClient.java
@@ -4,10 +4,14 @@ import kz.zhelezyaka.spring6Training_7.model.BeerDTO;
 import kz.zhelezyaka.spring6Training_7.model.BeerStyle;
 import org.springframework.data.domain.Page;
 
+import java.util.UUID;
+
 public interface BeerClient {
     Page<BeerDTO> listBeers();
 
     Page<BeerDTO> listBeers(String beerName, BeerStyle beerStyle,
                             Boolean showInventory, Integer pageNumber,
                             Integer pageSize);
+
+    BeerDTO getBeerById(UUID beerId);
 }
--- src/test/java/kz/zhelezyaka/spring6Training_7/client/BeerClientImplTest.java
+++ src/test/java/kz/zhelezyaka/spring6Training_7/client/BeerClientImplTest.java
@@ -1,14 +1,26 @@
 package kz.zhelezyaka.spring6Training_7.client;
 
+import kz.zhelezyaka.spring6Training_7.model.BeerDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class BeerClientImplTest {
 
     @Autowired
     BeerClientImpl beerClient;
+
+    @Test
+    void getBeerById() {
+        Page<BeerDTO> beerDTOS = beerClient.listBeers();
+        BeerDTO dto = beerDTOS.getContent().get(0);
+        BeerDTO byId = beerClient.getBeerById(dto.getId());
+        assertNotNull(byId);
+    }
 
     @Test
     void listBeersNoBeerName() {
