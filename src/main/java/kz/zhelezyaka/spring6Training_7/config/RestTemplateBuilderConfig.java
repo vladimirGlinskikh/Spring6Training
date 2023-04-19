f26ed18eb106abaf8419d8c4a130ade3764fc2d6
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-04-19 12:19:40 +0300

    HTTP Basic with RestTemplate

--- src/main/java/kz/zhelezyaka/spring6Training_7/config/RestTemplateBuilderConfig.java
+++ src/main/java/kz/zhelezyaka/spring6Training_7/config/RestTemplateBuilderConfig.java
@@ -13,6 +13,12 @@ public class RestTemplateBuilderConfig {
     @Value("${rest.template.rootUrl}")
     String rootUrl;
 
+    @Value("${rest.template.username}")
+    String username;
+
+    @Value("${rest.template.password}")
+    String password;
+
     @Bean
     RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer) {
 
@@ -21,6 +27,10 @@ public class RestTemplateBuilderConfig {
         RestTemplateBuilder builder = configurer.configure(new RestTemplateBuilder());
         DefaultUriBuilderFactory uriBuilderFactory =
                 new DefaultUriBuilderFactory(rootUrl);
-        return builder.uriTemplateHandler(uriBuilderFactory);
+
+        RestTemplateBuilder builderWithAuth =
+                builder.basicAuthentication(username, password);
+
+        return builderWithAuth.uriTemplateHandler(uriBuilderFactory);
     }
 }
