65e0959e0b3f9d3fdbee27b85f15afc7f868a655
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-04-19 16:07:02 +0300

    Create new project

---
+++ src/main/java/kz/zhelezyaka/spring6Training_8/SpringAuthServer.java
@@ -0,0 +1,11 @@
+package kz.zhelezyaka.spring6Training_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthServer {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAuthServer.class, args);
+    }
+}
